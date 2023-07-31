dd21f23a8c47cc1870ce56db618c3e0d2e89040c
Abdur Rahman <rahmanx661@gmail.com>
2023-07-31 09:04:32 +0600

    updated banner part

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -4,8 +4,7 @@ import './banner.css'
 const Banner = () => {
     return (
         <div className='lg:mb-44'>
-            <img className='lg:relative' src="https://www.pc-lux.com/images/banner/crop-1675659777597.jpg" alt="" />
-            <div className=' flex justify-center p-5 search-field rounded-lg'>
+            <div className=' flex justify-center p-5 search-field lg:rounded-lg z-10 lg:absolute lg:top-[450px] lg:w-[500px] lg:left-[450px]'>
                 <div className="form-control">
                     <div className='flex flex-row justify-center mb-5 gap-5'>
                         <h1 className='text-center text-xl font-bold text-white'>Buy</h1>
@@ -26,6 +25,7 @@ const Banner = () => {
                     </div>
                 </div>
             </div>
+            <img  src="https://www.pc-lux.com/images/banner/crop-1675659777597.jpg" alt="" />
         </div>
     );
 };
--- src/Page/Home/HomeBegin/HomeBegin.jsx
+++ src/Page/Home/HomeBegin/HomeBegin.jsx
@@ -15,7 +15,7 @@ const HomeBegin = () => {
                         <h1 className='text-2xl text-white '>Check out hot deals from TH’s leading property developers</h1>
                         <button className='btn  btn-error text-white mt-3 normal-case'>EasyOwn</button>
                     </div>
-                    <div className='p-7 bg-red-300 mt-5'>
+                    <div className='p-7 bg-red-300 lg:mt-0 mt-5'>
                         <h1 className='text-2xl text-black '>Thailand Property Market Outlook 2023</h1>
                         <button className='btn  btn-error text-white mt-3 normal-case'>Read More</button>
                     </div>
@@ -24,7 +24,7 @@ const HomeBegin = () => {
             {/* DDproperty EasyOwn */}
             <div className=''>
                 <h1 className='text-2xl font-semibold mb-3 mt-5 lg:mt-0'>DDproperty EasyOwn</h1>
-                <div className="hero lg:h-[456px]" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+                <div className="hero lg:h-[436px]" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
                     <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content  text-neutral-content">
                         <div className="max-w-md">
